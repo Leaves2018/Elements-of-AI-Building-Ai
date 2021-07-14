c990043b9f7038c617bb22ec03076789e419750b
yufeiyuan <yuanyufei1999@gmail.com>
2021-07-14 17:29:23 +0800

    update Exercise 12

--- Exercise 12-Least squares.py
+++ Exercise 12-Least squares.py
@@ -13,23 +13,12 @@ c = np.array([[3000, 200 , -50, 5000, 100],
 
 def find_best(X, y, c):
     smallest_error = np.Inf
-    best_index = 0
-    
-    x_and_c = []
-    for coeff in c:
-        x_and_c.append(X @ coeff)
-    x_and_X = np.array(x_and_c)
-    coefficient = []
-    for i in x_and_X:
-        coefficient.append([(n - j)**2 for n, j in zip(y, i)])
-    number = sum(coefficient[0])
-    for i in coefficient:
-        
-        if sum(i)<number:
-            number = sum(i)
-            best_index = coefficient.index(i)
-
-
+    best_index = -1
+    res = [sum((arr - y)**2) for arr in [X @ ci for ci in c]]
+    for i in range(len(res)):
+        if res[i] < smallest_error:
+            smallest_error = res[i]
+            best_index = i
     print("the best set is set %d" % best_index)
 
 
