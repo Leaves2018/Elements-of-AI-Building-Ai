885032a3d99932771b7953774eef51680c5ddf09
yufeiyuan <yuanyufei1999@gmail.com>
2021-07-19 11:33:40 +0800

    update Exercise 17

--- Exercise 17-Bag of words.py
+++ Exercise 17-Bag of words.py
@@ -1,21 +1,18 @@
 import numpy as np
+
 data = [[1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
         [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
         [1, 1, 1, 0, 1, 3, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1]]
+
 def find_nearest_pair(data):
     N = len(data)
-    dist = np.inf
+    data = np.array(data)
     dist = np.empty((N, N), dtype=np.float)
-    
     for i in range(N):
         for j in range(N):
-            sum = 0
-            for k in range(len(data[i])):
-                
-                sum += (data[j][k] - data[i][k])
-                dist[i][j] = sum
-    print(dist)
+            dist[i][j] = np.inf if i == j else sum(abs(data[j] - data[i]))
     print(np.unravel_index(np.argmin(dist), dist.shape))
+
 find_nearest_pair(data)
