3139d6b8f2912a5d7f36dfb3a4e804570f4a3dda
yufeiyuan <yuanyufei1999@gmail.com>
2021-07-19 11:07:26 +0800

    update Exercise 15

--- Exercise 15-Vector distances.py
+++ Exercise 15-Vector distances.py
@@ -10,23 +10,16 @@ def dist(a, b):
     return np.sqrt(sum)
     
 def nearest(x_train, x_test):
-    nearest = 0
+    nearest = -1
     min_distance = np.Inf
-    x = 0
-    min_distance = dist(x_test, x_train[0])
-    for i in x_train:
-        distance = dist(i, x_test)
-        if distance < 0:
-            abs(distance)
-        if min_distance > distance:
-            min_distance = distance
-            nearest = x
-        x+=1
-            
-    
     # add a loop here that goes through all the vectors in x_train and finds the one that
     # is nearest to x_test. return the index (between 0, ..., len(x_train)-1) of the nearest
     # neighbor
+    for i in range(len(x_train)):
+        temp_dist = dist(x_train[i], x_test)
+        if temp_dist < min_distance:
+            min_distance = temp_dist
+            nearest = i
     print(nearest)
 
 nearest(x_train, x_test)
