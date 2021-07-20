b5d17dffd231d3710602ee6565983929bcf3afd6
yufeiyuan <yuanyufei1999@gmail.com>
2021-07-20 11:18:57 +0800

    update Exercise 20

--- Exercise 20-Logistic regression.py
+++ Exercise 20-Logistic regression.py
@@ -6,11 +6,11 @@ c1 = np.array([-.5, .1, .08])
 c2 = np.array([-.2, .2, .31])
 c3 = np.array([.5, -.1, 2.53])
 
-def sigmoid(c1):
+def sigmoid(z):
     # add your implementation of the sigmoid function here
-    test = sum(c1)
-    print(test)
+    return 1 / (1 + np.exp(-z))
 
 # calculate the output of the sigmoid for x with all three coefficients
-
-sigmoid(c3)
+print(sigmoid(sum(x * c1))) # 0.15446526508353473
+print(sigmoid(sum(x * c2))) # 0.45016600268752216
+print(sigmoid(sum(x * c3))) # 0.8455347349164652
