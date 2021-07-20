e6ac788f91d895eeaf5f1493f9738cd7f67d1e6d
yufeiyuan <yuanyufei1999@gmail.com>
2021-07-20 10:55:02 +0800

    update Exercise 19

--- Exercise 19-Looking out for overfitting.py
+++ Exercise 19-Looking out for overfitting.py
@@ -11,37 +11,26 @@ x, y = make_moons(
     noise=0.3
 )
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
-print(x_train)
+
 # Create a classifier and fit it to our data
 knn = KNeighborsClassifier(n_neighbors=133)
 knn.fit(x_train, y_train)
 
-y_predict = np.empty(len(y_test), dtype=np.int64)
+# It seems the plotting job is done by ElementsOfAI
+# What we need to do in Exercise 19 is only using the knn.score function
+# Documents for sklearn.neighbors.KNeighborsClassifier: 
+# https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
+print("training accuracy: %f" % knn.score(x_train, y_train))
+print("testing accuracy: %f" % knn.score(x_test, y_test))
 
-lines = []
+# What would be a reasonable baseline accuracy your model should outperform in order for it to be considered useful?
+# 0.50
 
-def dist(a, b):
-    sum = 0
-    for ai, bi in zip(a, b):
-        sum = sum + (ai - bi)**2
-    return np.sqrt(sum)
+# Which of the following values of k do you think was "best"?
+# k=42
 
-def main(X_train, X_test, y_train, y_test):
-    global y_predict
-    global lines
-    k = 3
+# Why?
+# it gave the highest testing accuracy
 
-    for i, test_item in enumerate(X_test):
-
-        distances = [dist(train_item, test_item) for train_item in X_train]
-        nearest_indices = np.argsort(distances)
-        nearest_labels = y_train[nearest_indices[:k]]
-
-
-        y_predict[i] = np.round(np.mean(nearest_labels))
-    print(y_predict)
-
-main(x_train, y_train, x_test, y_test)
-
-print("training accuracy: %f" % 0.0)
-print("testing accuracy: %f" % 0.0)
+# Is it possible to have a higher test set accuracy than training set accuracy?
+# yes
